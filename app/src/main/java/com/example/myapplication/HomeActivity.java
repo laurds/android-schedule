f5eeab0a1e26edba918351292b27e2958be3ae20
Laura <laurarochamarchese@gmail.com>
2020-05-22 02:01:12 -0300

    Criação do app

---
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeScreen(View view) {
+
+        Intent nextActivity = new Intent(HomeActivity.this, SignUpActivity.class);
+        startActivity(nextActivity);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signUpLayout);
+    }
+
+    public void changeScreen(View view) {
+
+        Intent nextActivity = new Intent(SignUpActivity.this, HomeActivity.class);
+        startActivity(nextActivity);
+    }
+
+}
