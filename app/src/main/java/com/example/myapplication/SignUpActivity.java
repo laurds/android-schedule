7b4ea80003189bb2c95e8803256dace192c1fcb5
Laura <laurarochamarchese@gmail.com>
2020-06-20 03:58:36 -0300

    Alterações layout e splash screen

--- app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -11,7 +11,7 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signUpLayout);
+        setContentView(R.layout.activity_signup);
     }
 
     public void changeScreen(View view) {
--- app/src/main/java/com/example/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -5,26 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final int WAIT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getBaseContext(), HomeActivity.class));
+                Intent go = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(go);
                 finish();
             }
-        },5000);
+        },WAIT);
     }
 }
